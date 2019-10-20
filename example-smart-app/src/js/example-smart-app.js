e83c52af3070bb41b6ca27843023f730bc8afb40
Paul C. Martin <pmartin@gatech.edu>
2019-10-20 12:06:28 -0400

    Update to include weight

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+			      'http://loinc.org|29463-7']
                       }
                     }
                   });
@@ -39,6 +40,7 @@
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+	  var bodyweight = byCodes('29463-7');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
@@ -48,6 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
+          p.bodyweight = getQuantityValueAndUnit(bodyweight[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -83,6 +86,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      bodyweight: {value: ''},
     };
   }
 
@@ -126,6 +130,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#bodyweight').html(p.bodyweight);
   };
 
 })(window);
